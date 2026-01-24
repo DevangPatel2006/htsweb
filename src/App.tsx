d716a5d771e7ff84364538a98b3a4aa4478a37ae
DevangPatel2006 <girapatel361@gmail.com>
2026-01-24 17:42:20 +0530

    add vercel insights

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"; // Import this
+import { SpeedInsights } from "@vercel/speed-insights/react"; // Import this
 import Index from "./pages/Index";
 import Swag from "./pages/Swag";
 import NotFound from "./pages/NotFound";
@@ -15,14 +17,16 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-      <main role="main"> 
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/swag" element={<Swag />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-        
+        <main role="main"> 
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/swag" element={<Swag />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        {/* Add the components here to track all pages */}
+        <Analytics />
+        <SpeedInsights />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
